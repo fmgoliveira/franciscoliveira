236aa76aa40467cd7403979e6a7b9d4c3cb84547
dr-monocle <contact.drmonocle@gmail.com>
2023-04-14 11:56:43 +0100

    Added some more sections

--- pages/music.tsx
+++ pages/music.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 import styles from '@/styles/Music.module.scss'
 import heroImg from '@/assets/music_hero.png'
 import aboutImg from '@/assets/aboutMusic.jpg'
-import logo from '@/assets/logo_music.png'
-import { useEffect } from 'react'
+import logo from '@/assets/icon_music.png'
+import { useEffect, useState } from 'react'
 
 export default function Music() {
+  const [menuOpen, setMenuOpen] = useState(false);
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
@@ -35,30 +36,31 @@ export default function Music() {
         <link rel="icon" href="/favicon_music.png" />
       </Head>
       <main className={styles.main}>
-        {/* <header id="header">
-          <nav id="nav" className="row fadeUp">
-            <a href="#home"><Image src={logo} alt="" className="logo" /></a>
-            <ul className="list-items">
-              <a href="#aboutme"><li className="list-item fadeRight">About</li></a>
-              <a href="#aboutme"><li className="list-item fadeRight">Experience</li></a>
-              <a href="#portfolio"><li className="list-item fadeRight">Work</li></a>
-              <a href="#contact"><li className="list-item fadeRight">Contact</li></a>
-            </ul>
-
-
-          </nav>
-          <div className="open-mobile-nav"><i className="fa-solid fa-bars" id="open-nav"></i></div>
+        <header id="header">
+          <div>
+            <nav id="nav" className="row fadeUp">
+              <a href="#home" className='logo-container'><Image src={logo} alt="" className="logo" /></a>
+              <ul className="list-items">
+                <a href="#aboutme"><li className="list-item music fadeRight">About</li></a>
+                <a href="#aboutme"><li className="list-item music fadeRight">Experience</li></a>
+                <a href="#portfolio"><li className="list-item music fadeRight">Work</li></a>
+                <a href="#contact"><li className="list-item music fadeRight">Contact</li></a>
+              </ul>
+            </nav>
+            <div className="open-mobile-nav" onClick={() => setMenuOpen(true)}><i className="fa-solid fa-bars" id="open-nav"></i></div>
+          </div>
         </header>
 
         <div className="navigation-mobile">
-          <ul className="list-items-mobile">
-            <a href="#aboutme"><li className="list-item">About</li></a>
-            <a href="#aboutme"><li className="list-item">Experience</li></a>
-            <a href="#portfolio"><li className="list-item">Work</li></a>
-            <a href="#contact"><li className="list-item">Contact</li></a>
-            <div className="close-mobile-nav"><i className="fa-solid fa-xmark" id="close-nav"></i></div>
+          <ul className={"list-items-mobile" + (menuOpen ? ' active' : '')}>
+            <a href="#aboutme"><li className="list-item music">About</li></a>
+            <a href="#aboutme"><li className="list-item music">Experience</li></a>
+            <a href="#portfolio"><li className="list-item music">Work</li></a>
+            <a href="#contact"><li className="list-item music">Contact</li></a>
+            <div className="close-mobile-nav" onClick={() => setMenuOpen(false)}><i className="fa-solid fa-xmark" id="close-nav"></i></div>
           </ul>
-        </div> */}
+        </div>
+
         <section id="home" className={styles.home}>
           <div className='container'>
             <div className={styles.heroText + ' fadeUp'}>
@@ -72,53 +74,64 @@ export default function Music() {
         </section>
 
         <section id="aboutme" className="container fadeUp">
-          <h2 className={"section-h2 " + styles.headerLineDecoration}>About Me</h2>
-          <div className="row">
-            <div className={styles.aboutmeText + " fadeRight"}>
-              <p className={styles.aboutmeP}>Greetings, I&apos;m Michael Oliviera, a seasoned producer, musician, and composer. My passion for music has driven me to excel in my craft, and I bring a wealth of experience to every project I undertake.</p>
-              <p className={styles.aboutmeP}>As a producer, I&apos;ve honed my skills in crafting beats, manipulating sounds, and producing chart-topping tracks. My unique blend of creativity and technical expertise has allowed me to create music that stands out in a crowded industry.</p>
-              <p className={styles.aboutmeP}>As a musician, I&apos;m equally comfortable with writing and performing my own material, as well as lending my talents to others. My musical prowess has been honed over [number] years of experimentation with a variety of genres and styles.</p>
-              <p className={styles.aboutmeP}>I approach each project with a dedication to excellence, and my ultimate goal is to create music that moves and inspires others. I invite you to explore my website and experience the power of my music.</p>
-            </div>
-            <div>
-              <Image src={aboutImg} alt="" className={styles.aboutmeImg + ' fadeLeft'} />
+          <div>
+            <h2 className={"section-h2 " + styles.headerLineDecoration}>About Me</h2>
+            <div className="row">
+              <div className={styles.aboutmeText + " fadeRight"}>
+                <p className={styles.aboutmeP}>Francisco Oliveira is a musician and composer with a passion for creating music that captivates and inspires. With years of experience playing the trumpet (since he was 7) and piano, Francisco has developed a unique style that blends classical and contemporary influences.</p>
+                <p className={styles.aboutmeP}>As a teenager, Francisco began exploring his own compositional voice, starting with pop music and instrumental tracks that showcased his impressive technical ability and melodic sensibility. It wasn&apos;t long before he found his true passion in writing music for band and orchestra, crafting complex and emotionally rich scores that have drawn comparisons to the greatest film composers of all time.</p>
+                <p className={styles.aboutmeP}>Francisco&apos;s music has sweeping melodies, lush harmonies, and dynamic shifts in tone and mood that transport listeners to another world. He draws inspiration from a wide range of sources. The result is a sound that is uniquely his own, a sound that moves and inspires others.</p>
+              </div>
+              <div>
+                <Image src={aboutImg} alt="" className={styles.aboutmeImg + ' fadeLeft'} />
+              </div>
             </div>
           </div>
         </section>
 
-
-
-
-        <section id="portfolio">
-          <div className="container">
-            <h2 className="section-h2 header-line-decoration">My Work</h2>
-            <div className="fadeup">
+        <section id="portfolio" className="container fadeUp">
+          <div>
+            <h2 className={styles.headerLineDecoration}>My Work</h2>
+            <div className="fadeUp">
               <p>Welcome to my portfolio section, where I&apos;m excited to share some of my latest musical creations. As a producer, musician, and composer, I take pride in crafting songs that move and inspire listeners.</p>
               <p>In this section, you&apos;ll find a selection of my latest tracks, each with its own unique sound and story. I love exploring different genres and pushing my creative boundaries.</p>
-              <p>Thank you for considering my work, and I hope you enjoy the journey.</p>
+            </div>
+            <div className="portfoilo-container">
+              <div className={styles.portfolioBox + " row"}>
+                <div className={styles.portfolioBox + " fadeRight"}>
+                  <span>Fetaured Project</span>
+                  <p className={styles.projectName}>E-commerce</p>
+                  <div className={styles.projectDescriptionBox}>
+                    This e-commerce website was designed and developed by me using HTML, CSS, and JavaScript.The website is optimized for all devices and screen sizes, and it&apos;s easy to navigate through.
+                  </div>
+                  <div className={styles.techOfProjects}>
+                    <p>VS Code</p>
+                    <p>JavaScript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                  </div>
+                  <div className={'row ' + styles.linkOfProjects}>
+                    <a href="https://github.com/LedBajrami/boshop" target="_blank"><i className="fa-brands fa-github"></i></a>
+                    <a href="https://boshopp.netlify.app/" target="_blank"><i className="fa-solid fa-arrow-up-right-from-square"></i></a>
+                  </div>
+                </div>
+                <div className={styles.projectImg + " fadeLeft"}>
+                  {/* <img src="img/boshop.jpg" alt=""> */}
+                  <div className={styles.projectImgOverlay}></div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
 
-
-        <section id="contact" className="container row">
-          <div className="contact-text-box fadeRight">
-            <h2 className="header-line-decoration">CONTACT</h2>
-            <p>Thanks for visiting my website and exploring my music. If you have any questions, comments, or feedback, I would love to hear from you.</p>
-            <p>Please feel free to use the contact form below to reach out to me, and I&apos;ll do my best to respond as soon as possible. You can also follow me on social media to stay up-to-date with my latest music and projects.</p>
-            <p>I look forward to hearing from you and thank you for your interest in my music.</p>
-          </div>
-          <div className="contact-form fadeLeft">
-            <form action="mailto">
-              <h2>CONTACT FORM</h2>
-              <input type="text" placeholder="Name" />
-              <input type="text" placeholder="Telephone" />
-              <input type="email" placeholder="Email" />
-              <input type="text" placeholder="Message" /><br />
-              <button type="submit">Send Message</button>
-            </form>
-          </div>
+        <section id="contact" className="container fadeUp">
+          <span className="span">What&apos;s next?</span>
+          <p className="home-page-name">Get In Touch</p>
+          <p className="description-p">I&quot;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&quot;ll try my best to get back to you!</p>
+          <a href="mailto:ledibajrami8@gmail.com"><button className="resume-btn contact-btn">Say Hello</button></a>
+          <p className="builtby-p">Designed & Built by Led Bajrami</p>
         </section>
+
 
         <div className="socialFlex fadeUp">
           <div className="socials">
