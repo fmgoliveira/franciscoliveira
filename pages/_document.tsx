9ce399c0db73f2847a0b2c3ed1ad458d0c0c83f7
Francisco Oliveira <francisco.m.gomes.oliveira@gmail.com>
2023-05-17 16:06:19 +0100

    Finished music page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,8 +7,8 @@ export default function Document() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/brands.min.css" integrity="sha512-G/T7HQJXSeNV7mKMXeJKlYNJ0jrs8RsWzYG7rVACye+qrcUhEAYKYzaa+VFy6eFzM2+/JT1Q+eqBbZFSHmJQew==" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/fontawesome.min.css" integrity="sha512-giQeaPns4lQTBMRpOOHsYnGw1tGVzbAIHUyHRgn7+6FmiEgGGjaG0T2LZJmAPMzRCl+Cug0ItQ2xDZpTmEc+CQ==" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/brands.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/fontawesome.min.css" />
       </Head>
       <body>
         <Main />
--- pages/music/index.tsx
+++ pages/music/index.tsx
@@ -4,7 +4,6 @@ import styles from '@/styles/Music.module.scss'
 import aboutImg from '@/assets/aboutMusic.jpg'
 import logo from '@/assets/icon_music.png'
 import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
 
 import music1 from '@/assets/music1.jpg'
 import music3 from '@/assets/music3.jpg'
@@ -18,13 +17,14 @@ import music12 from '@/assets/music12.jpg'
 export default function Music() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [music, setMusic] = useState<{ id: string, name: string, image: string, url: string }[]>([]);
-  const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('/api/music')
       .then((response) => response.json())
       .then((data) => {
-        setMusic(data)
+        setMusic(data);
+        setLoading(false);
       })
   }, []);
 
@@ -44,7 +44,7 @@ export default function Music() {
     document.querySelectorAll('.fadeUp').forEach((el) => observer.observe(el));
     document.querySelectorAll('.fadeDown').forEach((el) => observer.observe(el));
     document.querySelectorAll('.fade').forEach((el) => observer.observe(el));
-  }, []);
+  }, [music]);
 
 
   return (
@@ -85,7 +85,7 @@ export default function Music() {
               <span>Hi, my name is</span>
               <h1 className={styles.name + " fadeRight"}>Francisco Oliveira</h1>
               <h2 className={styles.name + ' ' + styles.secondP}>Musician. Producer. Composer.</h2>
-              <p className={styles.descriptionP + " fadeLeft"}>Welcome to my official website, where you can find unique and engaging sounds that inspire and move people. So take a look around, listen to some of my work, and join me on this musical journey!</p>
+              <p className={styles.descriptionP + " fadeLeft"}>Welcome to my website, where you can find all about me. So take a look around, listen to some of my work, and join me on this musical journey!</p>
               <a href="#aboutme" className="btnMusic fadeRight">Explore more about me</a>
               <a href="#music" className="btnLink fadeLeft">
                 <i className="fa-solid fa-music"></i>
@@ -98,14 +98,19 @@ export default function Music() {
         <section id="aboutme" className="container fadeUp">
           <div>
             <h2 className={"section-h2 " + styles.headerLineDecoration}>About Me</h2>
-            <div className="row">
+            <div className={styles.aboutRow}>
               <div className={styles.aboutmeText + " fadeRight"}>
-                <p className={styles.aboutmeP}>Francisco Oliveira is a musician and composer with a passion for creating music that captivates and inspires. With years of experience playing the trumpet (since he was 7) and piano, Francisco has developed a unique style that blends classical and contemporary influences.</p>
-                <p className={styles.aboutmeP}>As a teenager, Francisco began exploring his own compositional voice, starting with pop music and instrumental tracks that showcased his impressive technical ability and melodic sensibility. It wasn&apos;t long before he found his true passion in writing music for band and orchestra, crafting complex and emotionally rich scores that have drawn comparisons to the greatest film composers of all time.</p>
-                <p className={styles.aboutmeP}>Francisco&apos;s music has sweeping melodies, lush harmonies, and dynamic shifts in tone and mood that transport listeners to another world. He draws inspiration from a wide range of sources. The result is a sound that is uniquely his own, a sound that moves and inspires others.</p>
+                <p className={styles.aboutmeP}>With no musical background in the family, I started, just like many others, in a philharmonic band, playing what is still today my favourite instrument - the trumpet.</p>
+                <p className={styles.aboutmeP}>It was probably in this environment that I got the inspiration to start composing... First listening, then reading, sometimes playing (😅), I ended up trying to write what others could also write. It wasn&apos;t easy to start - not even close - and sometimes it&apos;s hard to continue. I started by arranging pieces by other composers, until I found my own music.</p>
+                <p className={styles.aboutmeP}>Today I still play the trumpet and have learnt to &quot;use&quot; the piano to make my work easier. Most of my music is soundtracks and orchestral music, but in between are compositions for concert bands and classical music (both arrangements and original compositions).</p>
+                <p className={styles.aboutmeP}>I am not planning to stop now and, whether playing or composing, finding the right motivation, I know I will stay connected to this wonderful world that is the world of music.</p>
+                <p className={styles.aboutmeP}><em>And what about you? Why don&apos;t you try it too?</em></p>
               </div>
               <div>
-                <Image src={aboutImg} alt="" className={styles.aboutmeImg + ' fadeLeft'} />
+                <div className={styles.aboutMeImgContainer}>
+                  <Image src={aboutImg} alt="" className={styles.aboutmeImg + ' fadeLeft'} />
+                  <div className={styles.back}></div>
+                </div>
               </div>
             </div>
           </div>
@@ -118,31 +123,20 @@ export default function Music() {
           <Image src={music3} alt="" className={styles.photo + ' fadeDown'} />
         </div>
 
+
         <section id="music" className="container fadeUp">
           <div>
             <h2 className={styles.headerLineDecoration}>My Music</h2>
             <div className={styles.portfolioContainer}>
               {
-                music.length === 0 &&
-                <div className={styles.box + " fadeDown"} onClick={() => window.open('https://open.spotify.com/artist/4yBQABhPKupyNjFAUrQjkh', '_blank')}>
-                  <div className={styles.boxImg}>
-                    <Image src={logo} className={styles.imgImg} alt="" width={100} height={100} />
-                    <div className={styles.boxImgOverlay}>
-                      <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                    </div>
-                  </div>
-                  <div className={styles.boxText}>
-                    <p className={styles.boxTextP}>Loading...</p>
-                  </div>
-                </div>
+                // (loading || music.length === 0) && <p className='fadeDown'>Loading...</p>
               }
               {
-                music.map((a: any) => (
-                  <div className={styles.box + " fadeDown"} key={a.id} onClick={() => window.open(a.url, '_blank')}>
-                    <div className={styles.boxImg}>
-                      <Image src={a.image} className={styles.imgImg} alt="" width={100} height={100} />
+                !loading && music.map((a: any, i) => (
+                  <div className={styles.box + " fadeDown"} key={`music-${i}`} onClick={() => window.open(a.url, '_blank')}>
+                    <div className={styles.boxImg} style={{ backgroundImage: `url(${a.image})` }}>
                       <div className={styles.boxImgOverlay}>
-                        <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                        <i className="fa-solid fa-play"></i>
                       </div>
                     </div>
                     <div className={styles.boxText}>
@@ -162,10 +156,23 @@ export default function Music() {
           <Image src={music12} alt="" className={styles.photo + ' fadeUp'} />
         </div>
 
+        <section className={styles.socials}>
+          <span></span>
+          <div>
+            <a href="https://open.spotify.com/artist/4yBQABhPKupyNjFAUrQjkh" target="_blank"><i className="fa-brands fa-spotify"></i></a>
+            <a href="https://music.apple.com/us/artist/francisco-oliveira/1683345218" target="_blank"><i className="fa-brands fa-itunes"></i></a>
+            <a href="https://deezer.page.link/JDBUgujznCLZau6F9" target="_blank"><i className="fa-brands fa-deezer"></i></a>
+            <a href="https://youtube.com/@franciscooliveira_music" target="_blank"><i className="fa-brands fa-youtube"></i></a>
+            <a href="https://instagram.com/franciscooliveira.music" target="_blank"><i className="fa-brands fa-instagram"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100091631861736" target="_blank"><i className="fa-brands fa-facebook"></i></a>
+          </div>
+          <span></span>
+        </section>
+
         <section id="contact" className={"container fadeUp " + styles.contact}>
           <span className={styles.span}>What&apos;s next?</span>
           <p className={styles.name}>Get In Touch</p>
-          <p className={styles.descriptionP}>Whether you have a question or just want to say hi, I&quot;ll try my best to get back to you!</p>
+          <p className={styles.descriptionP}>Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!</p>
           <a href="mailto:franciscomgomesoliveira@gmail.com" className={styles.contactBtn + ' btnMusic'}>Say Hello</a>
         </section >
 
